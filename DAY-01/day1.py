6dd117866c03b99f4779e01604afd71207a9ecbd
SHAFRIN BARHANA.J <shafrinbarhana475@gmail.com>
2026-09-17 14:20:33 +0530

    Arithmetic-Operation
    Arithmetic-Operation

---
+++ DAY-01/day1.py
@@ -0,0 +1,15 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============================================================================================= RESTART: C:/Users/student114/Desktop/day1.py =============================================================================================
+Enter first number: 20
+Enter second number: 10
+
+Arithmetic Operations:
+Addition       = 30.0
+Subtraction    = 10.0
+Multiplication = 200.0
+Division       = 2.0
+Floor Division = 2.0
+Modulus        = 0.0
+Exponentiation = 10240000000000.0
